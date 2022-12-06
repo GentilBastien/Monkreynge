6d78948b81e7ccf9d8692381f67978da271b48b2
GentilBastien <bastien.gattaciecca@gmail.com>
2022-12-06 16:37:23 +0100

    Navbar

--- monkreynge-app/src/App.vue
+++ monkreynge-app/src/App.vue
@@ -1,16 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <div>
       <nav>
-        <RouterLink to="/">Monkreynge</RouterLink>
-        <RouterLink to="/contenu">Contenu</RouterLink>
-        <RouterLink to="/conversations">Conversations</RouterLink>
-        <RouterLink to="/profil">Profil</RouterLink>
+        <RouterLink to="/">
+          <div id="home">
+            <span>M </span>
+            <img id="monk" src="@/components/icons/monk.png" alt="contenu">
+            <span>NKrEYnge</span>
+          </div>
+        </RouterLink>
+        <RouterLink to="/contenu">
+          <div>
+            <img src="@/components/icons/contenu.png" alt="contenu">
+            <span>Contenu</span>
+          </div>
+        </RouterLink>
+        <RouterLink to="/conversations">
+          <div>
+            <img src="@/components/icons/mail.png" alt="contenu">
+            <span>Conversations</span>
+          </div>
+        </RouterLink>
+        <RouterLink to="/profil">
+          <div>
+            <img src="@/components/icons/user.png" alt="contenu">
+            <span>Profil</span>
+          </div>
+        </RouterLink>
       </nav>
     </div>
   </header>
@@ -18,5 +38,52 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+header {
+  background-color: var(--main-col);
+  height: 100px;
+  overflow: hidden;
+}
 
+a div#home {
+  display: inline-block;
+}
+
+a div#home img {
+  height: 60px;
+}
+
+a div#home span {
+  color: white;
+  font-style: italic;
+  font-size: 80px;
+  font-family: "Tapestry";
+}
+
+a div:not(#home) {
+  float: right;
+  width: 100px;
+  padding: 5px;
+  margin: 10px;
+}
+
+a div:not(#home) img {
+  width: 40px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+a div:not(#home) span {
+  display: block;
+  text-align: center;
+  color: white;
+}
+
+nav a.router-link-exact-active {
+
+}
+
+nav a.router-link-exact-active:hover {
+
+}
 </style>
--- monkreynge-app/src/views/ContentView.vue
+++ monkreynge-app/src/views/ContentView.vue
@@ -1,3 +1,9 @@
 <template>
-    
+    <div>
+        
+    </div>
 </template>
+
+<style scoped>
+    
+</style>
